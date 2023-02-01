c31ae4d134ea091a0e01167f2a9184a540c9d109
Joshua Hall <joshdhal@amazon.com>
2023-02-01 19:55:22 +0000

    Fix prod build

--- publish-resource-regions.sh
+++ publish-resource-regions.sh
@@ -4,10 +4,10 @@
 #
 # Run this from the resource folder
 
-regions_to_publish=(us-east-1 af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ap-southeast-3 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-central-1 me-south-1 sa-east-1 us-east-2 us-west-1 us-west-2 eu-central-2 eu-south-2 ap-south-2)
+#regions_to_publish=(us-east-1 af-south-1 ap-east-1 ap-northeast-1 ap-northeast-2 ap-northeast-3 ap-south-1 ap-southeast-1 ap-southeast-2 ap-southeast-3 ca-central-1 eu-central-1 eu-north-1 eu-south-1 eu-west-1 eu-west-2 eu-west-3 me-central-1 me-south-1 sa-east-1 us-east-2 us-west-1 us-west-2 eu-central-2 eu-south-2 ap-south-2)
 
 # Use this to test succeed-fail locally
-#regions_to_publish=(us-east-1 us-seattle)
+regions_to_publish=(us-east-1 us-west-2)
 
 successes=()
 failures=()
@@ -45,8 +45,9 @@ for region in ${regions_to_publish[@]}
 do
     echo "About to start publishing to $region"
 
-    ../../release/deregister-all.sh $region RESOURCE
-    ../../release/publish-resource.sh $region
+    ls -al
+    ./deregister-all.sh $region RESOURCE
+    ./publish-resource.sh $region
 
     if [ "$?" -eq 0 ]
     then
